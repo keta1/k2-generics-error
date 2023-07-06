b6f9b794b7ae761dfa87b7a6a4838ef4ec1e9575
keta1 <k@ketal.icu>
2023-07-06 19:58:13 +0800

    Add workflows

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,17 @@ pluginManagement {
 }
 
 plugins {
+    `gradle-enterprise`
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
 }
 
 rootProject.name = "k2-generics-error"
