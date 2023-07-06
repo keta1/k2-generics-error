da5c0293826a8c802e074ebbfa33be5fdcae4f56
keta1 <k@ketal.icu>
2023-07-06 21:43:34 +0800

    add normal example

---
+++ src/main/kotlin/b/Work.kt
@@ -0,0 +1,15 @@
+package b
+
+import kotlin.properties.ReadOnlyProperty
+
+fun main() {
+    val a: Int? = "1".toIntOrNull()
+    val b by test(a)
+    printInt(b)
+}
+fun <T> test(value: T): ReadOnlyProperty<Nothing?, T & Any> =
+    ReadOnlyProperty { _, _ -> value!! }
+
+fun printInt(i: Int) {
+    println(i)
+}
