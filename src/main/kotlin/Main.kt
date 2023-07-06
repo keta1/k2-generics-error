bc8cc58a1aa717e3d65ace0480782f1e54f787c5
keta1 <k@ketal.icu>
2023-07-06 21:29:01 +0800

    Format code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 import kotlin.properties.ReadOnlyProperty
 
 fun main() {
-    val a: Int? = 1
+    val a: Int? = "1".toIntOrNull()
     val b by test { a }
     printInt(b)
 }
- fun <T> test(value: () -> T): ReadOnlyProperty<Nothing?, T & Any> =
-    ReadOnlyProperty { _, _ -> value()!! }
+
+fun <T> test(value: () -> T): ReadOnlyProperty<Nothing?, T & Any> =
+    ReadOnlyProperty { _, _ -> value() ?: throw Error() }
+
 fun printInt(i: Int) {
     println(i)
 }
